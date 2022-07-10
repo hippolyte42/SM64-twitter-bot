1d38bcc47cbe23282ac6a4f73adc8c2a02f47e64
hippolyte42 <hippolytejacque@hotmail.fr>
2022-07-10 17:36:10 +0200

    isShorterDuration fix

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -21,53 +21,48 @@ export const isShorterDuration = (
   if (isDurationInHour(duration1) && !isDurationInHour(duration2)) {
     return false;
   } else if (isDurationInHour(duration2)) {
-    const segment1Hours = parseInt(
-      duration1.slice(0, duration1.indexOf(":")),
-      10
+    const segment1Hours = parseFloat(
+      duration1.slice(0, duration1.indexOf(":"))
     );
-    const segment2Hours = parseInt(
-      duration2.slice(0, duration2.indexOf(":")),
-      10
+    const segment2Hours = parseFloat(
+      duration2.slice(0, duration2.indexOf(":"))
     );
     const isShorterHour =
       isShorterSegment(segment1Hours, segment2Hours) ||
       !isDurationInHour(duration1);
+
     if (isShorterHour) {
       return true;
     }
   }
 
-  const segment1Minutes = parseInt(
+  const segment1Minutes = parseFloat(
     isDurationInHour(duration2)
       ? duration1.slice(
-          duration1.indexOf(":"),
+          duration1.indexOf(":") + 1,
           duration1.indexOf(":", duration1.indexOf(":") + 1)
         )
-      : duration1.slice(0, duration1.indexOf(":")),
-    10
+      : duration1.slice(0, duration1.indexOf(":"))
   );
-  const segment2Minutes = parseInt(
+  const segment2Minutes = parseFloat(
     isDurationInHour(duration2)
       ? duration2[3] + duration2[4]
-      : duration2[0] + duration2[1],
-    10
+      : duration2[0] + duration2[1]
   );
 
   const isShorterMinute = isShorterSegment(segment1Minutes, segment2Minutes);
   if (isShorterMinute) {
     return true;
-  } else if (!isShorterMinute) {
+  } else if (isShorterMinute === false) {
     return false;
   }
-  const duration1Seconds = parseInt(
-    duration1.slice(duration1.indexOf(":", duration1.indexOf(":") + 1)),
-    10
+  const duration1Seconds = parseFloat(
+    duration1.slice(duration1.indexOf(":") + 1)
   );
-  const duration2Seconds = parseInt(
+  const duration2Seconds = parseFloat(
     isDurationInHour(duration2)
       ? duration2[6] + duration2[7]
-      : duration2[3] + duration2[4],
-    10
+      : duration2[3] + duration2[4]
   );
 
   const isShorterSeconds = isShorterSegment(duration1Seconds, duration2Seconds);
