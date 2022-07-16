fb27282dfb76eac7a9069e81d549ae8abfead6b7
hippolyte42 <hippolytejacque@hotmail.fr>
2022-07-17 00:43:09 +0200

    handle !response.ok for speedrunApiUtils

--- src/utils/speedrunApiUtils.ts
+++ src/utils/speedrunApiUtils.ts
@@ -5,34 +5,52 @@ import { getTwitterSlugFromUri, ISO8601durationToDigital } from "./formatUtils";
 import { isShorterDuration } from "./timeUtils";
 
 export const getPlayerName = async (playerId: string) => {
-  const response = await fetch(
-    `https://www.speedrun.com/api/v1/users/${playerId}`
-  );
-  return ((await response.json()) as any).data.names.international;
+  const url = `https://www.speedrun.com/api/v1/users/${playerId}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.log(`Request returned ${response.status} from ${url}`, response);
+    return;
+  }
+
+  const playerName = ((await response.json()) as any).data.names.international;
+
+  return playerName;
 };
 
 export const getPlayerTwitter = async (playerId: string) => {
-  const response = await fetch(
-    `https://www.speedrun.com/api/v1/users/${playerId}`
-  );
+  const url = `https://www.speedrun.com/api/v1/users/${playerId}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.log(`Request returned ${response.status} from ${url}`, response);
+    return;
+  }
+
   const twitterUri: string | undefined = ((await response.json()) as any).data
     .twitter?.uri;
   return twitterUri ? getTwitterSlugFromUri(twitterUri) : undefined;
 };
 
 export const getCategory = async (categoryId: string) => {
-  const response = await fetch(
-    `https://www.speedrun.com/api/v1/categories/${categoryId}/records?status=verified`
-  );
+  const url = `https://www.speedrun.com/api/v1/categories/${categoryId}/records?status=verified&${uuidv4()}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.log(`Request returned ${response.status} from ${url}`, response);
+    return;
+  }
+
   const categoryData = await response.json();
 
   return categoryData;
 };
 
 const getPlatformName = async (platformId: string) => {
-  const response = await fetch(
-    `https://www.speedrun.com/api/v1/platforms/${platformId}`
-  );
+  const url = `https://www.speedrun.com/api/v1/platforms/${platformId}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.log(`Request returned ${response.status} from ${url}`, response);
+    return;
+  }
+
   const platformName = (await response.json()).data.name;
 
   return platformName;
@@ -42,36 +60,40 @@ export const getNewNoteworthyRuns = async (
   categoryInfo: CategoryInfo,
   NoteworthyRunsData: NoteworthyRunsData[]
 ): Promise<NoteworthyRunsData[]> => {
-  const response = await fetch(
-    `https://www.speedrun.com/api/v1/runs?category=${categoryInfo.id}&orderby=verify-date&direction=desc`
-  );
+  const url = `https://www.speedrun.com/api/v1/runs?status=verified&category=${
+    categoryInfo.id
+  }&orderby=verify-date&direction=desc&${uuidv4()}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.log(`Request returned ${response.status} from ${url}`, response);
+    return;
+  }
+
   const categoryNewVerifiedRunsData = (await response.json()).data;
 
   const newNoteworthyRuns: NoteworthyRunsData[] = [];
 
   for (let newVerifiedRun of categoryNewVerifiedRunsData) {
-    if (newVerifiedRun.status.status === "verified") {
-      const runnerTime: string | undefined =
-        newVerifiedRun.times.realtime &&
-        ISO8601durationToDigital(newVerifiedRun.times.realtime);
-      if (
-        runnerTime &&
-        isShorterDuration(runnerTime, categoryInfo.noteworthyTime) &&
-        !NoteworthyRunsData.some(
-          async (v) =>
-            v.runnerName ===
-              (await getPlayerName(newVerifiedRun.players[0].id)) &&
-            v.runnerTime === runnerTime
-        )
-      ) {
-        newNoteworthyRuns.push({
-          runnerName: await getPlayerName(newVerifiedRun.players[0].id),
-          runnerTime,
-          runnerId: newVerifiedRun.players[0].id,
-          runWeblink: newVerifiedRun.weblink,
-          runPlatform: await getPlatformName(newVerifiedRun.system.platform),
-        });
-      }
+    const runnerTime: string | undefined =
+      newVerifiedRun.times.realtime &&
+      ISO8601durationToDigital(newVerifiedRun.times.realtime);
+    if (
+      runnerTime &&
+      isShorterDuration(runnerTime, categoryInfo.noteworthyTime) &&
+      !NoteworthyRunsData.some(
+        async (v) =>
+          v.runnerName ===
+            (await getPlayerName(newVerifiedRun.players[0].id)) &&
+          v.runnerTime === runnerTime
+      )
+    ) {
+      newNoteworthyRuns.push({
+        runnerName: await getPlayerName(newVerifiedRun.players[0].id),
+        runnerTime,
+        runnerId: newVerifiedRun.players[0].id,
+        runWeblink: newVerifiedRun.weblink,
+        runPlatform: await getPlatformName(newVerifiedRun.system.platform),
+      });
     }
   }
 
